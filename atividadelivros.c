e059aceb62a4ca309b8a50135983b67378225f01
MiguelHzUnicap <132664790+MiguelHzUnicap@users.noreply.github.com>
2025-05-31 11:40:55 -0300

    atividadelivros.c

--- atividadelivros.c
+++ atividadelivros.c
@@ -70,3 +70,59 @@ void imprimirLivro(struct Livro livro) {
     livro.anoPublicacao,
   );
 }
+
+int buscarPelotTitulo(struct Livro v[], int tam, char *titulo) {
+  for (int i = 0; i < tam; i += 1) {
+    if (strcmp(v[i].titulo, titulo) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void ordenarLivroBubblesort (Struct Livro livro, int n) {
+    int i, fim, pos, troca;
+    float chave;
+    
+    troca = 1; fim = n - 2; pos = 0;
+    while (troca == 1) {
+        troca = 0;
+        for (i = 0; i <= fim; i++) {
+            if (livro.titulo[i] > livro.titulo[i+1]) {
+                chave = v[i]; v[i] = v[i+1]; v[i+1] = chave;
+                pos = i; troca = 1;
+            }
+        }
+        fim = pos-1;
+    }
+}
+
+//int buscaBinariaTitulo(int v[], int n, int x) {
+  //int inicio = 0, fim = n - 1, meio;
+  //while (inicio <= fim) {
+    //meio = (inicio + fim) / 2;
+    //if (strcmp(v[meio].titulo == x) {
+      //return meio;
+    //} else if (x < strcmp(v[meio].titulo) {
+      //fim = meio - 1;
+    //} else {
+      //inicio = meio + 1;
+    //}
+  //}
+  //return -1;
+//}
+
+int buscaBinariaTitulo(struct Livro v[], int tam, char *x) {
+  int inicio = 0, fim = tam - 1, meio;
+  while (inicio <= fim) {
+    meio = (inicio + fim) / 2;
+    if (strcmp(v[meio].titulo, x) > 0) {
+      fim = meio - 1;
+    } else if (strcmp(v[meio].titulo, x) < 0) {
+      inicio = meio + 1;
+    } else {
+      return meio;
+    }
+  }
+  return -1;
+}
